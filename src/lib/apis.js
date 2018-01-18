82f8cf9369efb313b1a7035fc984fc07dccab7ad
duyinghua <duyinghua881015>
2018-01-19 01:48:34 +0800

    tijiao

--- src/lib/apis.js
+++ src/lib/apis.js
@@ -1,26 +1,13 @@
 import {ENV, SSOHOST, APIHOST, IMAGEHOST} from '../lib/env.js'
 const config = {
-  apiHost: APIHOST[ENV],
-  ssoHost: SSOHOST[ENV],
-  imageHost: IMAGEHOST[ENV]
+    apiHost: APIHOST[ENV],
+    ssoHost: SSOHOST[ENV],
+    imageHost: IMAGEHOST[ENV]
 }
 
 export default {
-  login: `${config.ssoHost}/user/oauth/lite/login`,
-  uploadImage: `${config.imageHost}/upload/v2/direct?scope=koudai&fileType=image`,
-  mine: `${config.apiHost}/fxx/homepage/main`,//我的
-  myUserComment: `${config.apiHost}/fxx/user_comment/list_new`, //我的评价列表
-  favoriteGoods: `${config.apiHost}/fxx/homepage/favorite/stu`, // 我收藏的商品
-  setFavorite: `${config.apiHost}/fxx/udc/favorite`,// 收藏商品
-  categoryList: `${config.apiHost}/fxx/category/main`,  // 榜单品类
-  categorySub: `${config.apiHost}/fxx/category/sub`,  // 榜单二级品类
-  getGoodsListSort: `${config.apiHost}/fxx/item/orderedCategory/list`,  // 品类商品列表
-  getGoodsData: `${config.apiHost}/fxx/item/rush/detail`, //商品详情
-  getGoodsComment: `${config.apiHost}/fxx/udc/third_comment/detail`, //商品评价
-  addUserComment: `${config.apiHost}/fxx/udc/user_comment/create_new`, //添加评价
-  delUserComment: `${config.apiHost}/fxx/udc/user_comment/delete`, //删除评价
-  likeComment: `${config.apiHost}/fxx/forum/like`, //点赞评价
-  getSuggestList: `${config.apiHost}/fxx/suggestion/word`,//搜索联想
-  getSearchData: `${config.apiHost}/fxx/evaluation/item_search_category`
+    login: `${config.apiHost}/wap/redpackagelite/user/login`,
+    qiniuToken: `${config.apiHost}/open/qiniu/token/redpackagelite?data=%7B%22cur_uid%22%3A3254493%7D`,///open/qiniu/wap/image/token?data=%7B%22cur_uid%22%3A3254493%7D
+    mine: `${config.apiHost}/fxx/homepage/main`,//我的
 
 }
--- src/lib/env.js
+++ src/lib/env.js
@@ -1,29 +1,26 @@
 const ENV = {
-  dev: 'develop',
-  pre: 'beta',
-  prod: 'stable'
+    dev: 'develop',
+    pre: 'beta',
+    prod: 'stable'
 }.prod;
 
 const SSOHOST = {
-  develop: 'https://sso-daily.test.weidian.com',
-  beta: 'https://sso-pre.test.weidian.com',
-  stable: 'https://sso.weidian.com'
+    develop: 'https://sso-daily.test.weidian.com',
+    stable: 'https://sso.weidian.com'
 }
 
 const APIHOST = {
-  develop: 'https://gwh5.test.api.fangxin.com',
-  beta: 'https://gwh5.test.api.fangxin.com',
-  stable: 'https://gwh5.api.fangxin.com'
+    develop: 'https://api.immocha.com',
+    stable: 'https://develop.immocha.com'
 }
 
 const IMAGEHOST = {
-  develop: 'https://mediaapi.daily.weidian.com',
-  beta: 'https://mediaapi.pre.weidian.com',
-  stable: 'https://media.api.weidian.com'
+    develop: 'https://mediaapi.daily.weidian.com',
+    stable: 'https://media.api.weidian.com'
 }
 
 const WEBVIEW_HOST = {
-  fangxin: 'https://m.fangxin.com'
+    fangxin: 'https://m.fangxin.com'
 }
 
 export {ENV, SSOHOST, APIHOST, IMAGEHOST, WEBVIEW_HOST}
